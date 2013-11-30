d929b1a32b182f0229512b35d46e0e3f45212c8f
aceberg <lin3d.ru@gmail.com>
2013-11-30 21:20:11 +0700

    Changes made to fit new OW API

--- openweather.py
+++ openweather.py
@@ -7,91 +7,94 @@ import os.path
 import ConfigParser
 import sys
 
-class getWeather():
 
+class getWeather():
     def get_city_id(self, city):
-    
+
         city_list = ConfigParser.ConfigParser()
         city_list.read('./city-id.txt')
 
         try:
-            city_id = city_list.get('CityList',city)
-        except:    
-            url = "http://openweathermap.org/data/2.0/find/name?q="+city
+            city_id = city_list.get('CityList', city)
+        except:
+            url = "http://openweathermap.org/data/2.0/find/name?q=" + city
             try:
                 f = urllib2.urlopen(url)
             except:
                 print 'Error!'
-            s = f.read()   
-            
+            s = f.read()
+
             city_id = s.split("\"id\":")[-1].split(",")[0]
-            
-            if str(city_id).isdigit():   
-                output = open('./city-id.txt','a')
-                output.write('\n'+city+" = "+city_id)
+
+            if str(city_id).isdigit():
+                output = open('./city-id.txt', 'a')
+                output.write('\n' + city + " = " + city_id)
                 output.close()
             else:
                 print 'Error! There is no such city!'
                 sys.exit()
-                    
+
         return city_id
 
 
-    def get_json_data(self,city):
-        
+    def get_json_data(self, city):
+
         city_id = self.get_city_id(city)
-            
-        url = "http://openweathermap.org/data/2.0/weather/city/"+city_id+"?type=json"
+
+        #url = "http://openweathermap.org/data/2.0/weather/city/" + city_id + "?type=json"
+        url = "http://api.openweathermap.org/data/2.5/weather?id=" + city_id
         try:
             f = urllib2.urlopen(url)
         except:
             return None
-        s = f.read()           
+        s = f.read()
         return s
 
-        
+
     def get_weather(self, city):
-        
+
         json_data = self.get_json_data(city)
         jdata_decoded = json.loads(json_data)
         w_data = {}
-        
+
         w_data['id'] = str(jdata_decoded['id'])
         w_data['name'] = str(jdata_decoded['name'])
-        w_data['date'] = str(jdata_decoded['date'])
-        w_data['temp'] = str(jdata_decoded['main']['temp'] - 273.15)+""u"\u00B0"
-        w_data['pressure'] = str(jdata_decoded['main']['pressure']*0.75)+" mmHg"
-        w_data['humidity'] = str(jdata_decoded['main']['humidity'])+"%"
-        w_data['wind'] = str(jdata_decoded['wind']['speed'])+" m/s"
-        w_data['clouds'] = str(jdata_decoded['clouds']['all'])+"%"
-        w_data['rain'] = str(jdata_decoded['rain']['3h'])
-        
-        w_img = jdata_decoded['img']
-        w_iconname = w_img.split("/")[-1]
-        
-        if not os.path.exists('./images/'+w_iconname):
-            w_icon = urllib2.urlopen(w_img).read()
-            output = open('./images/'+w_iconname,'wb')
-            output.write(w_icon)
-            output.close()
-         
-        w_data['img'] = str(w_iconname)
-        
-        return w_data    
-    
+        #w_data['date'] = str(jdata_decoded['date'])
+        w_data['temp'] = str(jdata_decoded['main']['temp'] - 273.15) + ""u"\u00B0"
+        w_data['pressure'] = str(jdata_decoded['main']['pressure'] * 0.75) + " mmHg"
+        w_data['humidity'] = str(jdata_decoded['main']['humidity']) + "%"
+        w_data['wind'] = str(jdata_decoded['wind']['speed']) + " m/s"
+        w_data['clouds'] = str(jdata_decoded['clouds']['all']) + "%"
+        # TODO: rain and snow in one variable
+        w_data['rain'] = str(jdata_decoded['snow']['3h'])
+
+        #w_img = jdata_decoded['img']
+        #w_iconname = w_img.split("/")[-1]
+
+        #w_iconname = str(json.loads(jdata_decoded['weather'])['icon'])
+
+        #if not os.path.exists('./images/' + w_iconname):
+        #    w_icon = urllib2.urlopen(w_img).read()
+        #    output = open('./images/' + w_iconname, 'wb')
+        #    output.write(w_icon)
+        #    output.close()
+
+        #w_data['img'] = str(w_iconname)
+
+        return w_data
+
     def main(self, city):
-        
+
         weather_data = self.get_weather(city)
-        print 'Weather in '+city+''
-        print 'Temperature:',weather_data['temp']
-        print 'Pressure   :',weather_data['pressure']
-        print 'Humidity   :',weather_data['humidity']
-        print 'Wind       :',weather_data['wind']
-        print 'Clouds     :',weather_data['clouds']
-        print 'Rain       :',weather_data['rain']
-        
+        print 'Weather in ' + city + ''
+        print 'Temperature:', weather_data['temp']
+        print 'Pressure   :', weather_data['pressure']
+        print 'Humidity   :', weather_data['humidity']
+        print 'Wind       :', weather_data['wind']
+        print 'Clouds     :', weather_data['clouds']
+        print 'Rain       :', weather_data['rain']
+
 
 if __name__ == "__main__":
-
     app = getWeather()
     app.main(str(sys.argv[1]))
--- weather_widget.py
+++ weather_widget.py
@@ -14,8 +14,6 @@ city = 'Novosibirsk'
 ################
 
 class WeatherApp:
-
-
     def __init__(self):
 
         self.statusicon = gtk.StatusIcon()
@@ -24,92 +22,90 @@ class WeatherApp:
         self.statusicon.connect("activate", self.left_click_event)
 
         self.update()
-        gobject.timeout_add(5*60*1000,self.update)
-       
-        
+        gobject.timeout_add(5 * 60 * 1000, self.update)
+
+
     def right_click_event(self, icon, button, time):
         menu = gtk.Menu()
 
         about = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
-        upd = gtk.ImageMenuItem(gtk.STOCK_REFRESH) 
+        upd = gtk.ImageMenuItem(gtk.STOCK_REFRESH)
         forecast = gtk.ImageMenuItem("Прогноз")
         img = gtk.Image()
-        img.set_from_stock(gtk.STOCK_YES,gtk.ICON_SIZE_MENU)
+        img.set_from_stock(gtk.STOCK_YES, gtk.ICON_SIZE_MENU)
         forecast.set_image(img)
         edit = gtk.ImageMenuItem(gtk.STOCK_PREFERENCES)
         seprt = gtk.SeparatorMenuItem()
         quit = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-        
+
         about.connect("activate", self.show_about_dialog)
         upd.connect("activate", self.upd_update)
         forecast.connect("activate", self.show_forecast)
         edit.connect("activate", self.show_edit_dialog)
         quit.connect("activate", gtk.main_quit)
-        
+
         menu.append(about)
         menu.append(upd)
         menu.append(forecast)
         menu.append(edit)
         menu.append(seprt)
         menu.append(quit)
-        
+
         menu.show_all()
-        
+
         menu.popup(None, None, gtk.status_icon_position_menu, button, time, self.statusicon)
-    
-        
-    def show_edit_dialog(self,widget):
-    
+
+
+    def show_edit_dialog(self, widget):
+
         window = gtk.Window()
         window.set_title("GtkWeather")
         #window.set_default_size(250, 200)
         #window.set_icon_from_stock(gtk.STOCK_PREFERENCES,10)
         window.set_border_width(10)
-        
+
         box1 = gtk.VBox(gtk.TRUE, 5)
         window.add(box1)
         box1.show()
-        
-        
+
         comboboxentry = gtk.combo_box_entry_new_text()
         #window.add(comboboxentry)
         comboboxentry.set_tooltip_text("Choose your sity")
-        
-        f = open("city-id.txt","r")
+
+        f = open("city-id.txt", "r")
         s = f.readline()
         while s:
             s = f.readline()
             item = s.split(' ')[0]
             if item != '':
                 comboboxentry.append_text(item)
-        
+
         comboboxentry.child.connect('changed', self.changed_city)
         comboboxentry.set_active(0)
-        
+
         box1.pack_start(comboboxentry, gtk.FALSE, gtk.FALSE, 0)
-        
+
         window.show_all()
-        
-        
-        
+
+
     def changed_city(self, entry):
-        
+
         global city
         city = unicode(entry.get_text())
         print 'Выбран город', city, '.'
-        
+
         self.update()
         return
-        
+
     def show_forecast(self, widget):
         window = gtk.Window()
         window.set_title("GtkWeather")
         window.set_default_size(250, 200)
         window.set_border_width(10)
-        
+
         window.show_all()
-        
-    
+
+
     def show_about_dialog(self, widget):
         about_dialog = gtk.AboutDialog()
 
@@ -117,51 +113,48 @@ class WeatherApp:
         about_dialog.set_name("GtkWeather")
         about_dialog.set_version("1.0")
         about_dialog.set_authors(["Ace-berg"])
-        		
+
         about_dialog.run()
         about_dialog.destroy()
 
     def left_click_event(self, data=None):
-        
+
         app = getWeather()
         weather_data = app.get_weather(city)
-        
+
         window = gtk.Window()
         window.set_title("GtkWeather")
         window.set_default_size(250, 200)
         window.set_border_width(10)
-        
+
         image = gtk.Image()
-        image.set_from_file('images/'+weather_data['img'])
-        
-        weather = 'Weather in '+weather_data['name']+'\n\nTemperature: '+weather_data['temp']\
-                +'\nPressure      : '+weather_data['pressure']+'\nHumidity      : '+weather_data['humidity']\
-                +'\nWind             : '+weather_data['wind']+'\nClouds          : '+weather_data['clouds']\
-                +'\nRain              : '+weather_data['rain']
+        #image.set_from_file('images/' + weather_data['img'])
+
+        weather = 'Weather in ' + weather_data['name'] + '\n\nTemperature: ' + weather_data['temp'] \
+                  + '\nPressure      : ' + weather_data['pressure'] + '\nHumidity      : ' + weather_data['humidity'] \
+                  + '\nWind             : ' + weather_data['wind'] + '\nClouds          : ' + weather_data['clouds'] \
+                  + '\nRain              : ' + weather_data['rain']
         text = gtk.Label(weather)
-        
-        fix = gtk.Fixed()   
+
+        fix = gtk.Fixed()
         fix.put(image, 0, 0)
         fix.put(text, 40, 50)
-        
+
         window.add(fix)
-        
-        
-        
+
         window.show_all()
-        
 
 
     def update(self):
-        
+
         app = getWeather()
         weather_data = app.get_weather(city)
-        
-        self.statusicon.set_tooltip(weather_data['name']+', '+weather_data['temp'])
-        self.statusicon.set_from_file('images/'+weather_data['img'])
-        
+
+        self.statusicon.set_tooltip(weather_data['name'] + ', ' + weather_data['temp'])
+        #self.statusicon.set_from_file('images/' + weather_data['img'])
+
         return 1
-        
+
     def upd_update(self, widget):
 
         self.update()
@@ -177,6 +170,5 @@ class WeatherApp:
 ##################
 
 if __name__ == "__main__":
-
     app = WeatherApp()
     app.main()
